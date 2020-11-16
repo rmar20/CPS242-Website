818acd0ec7a3f722dad6832bd49cf95a62f369a6
Unknown <marr01@udayton.edu>
2020-11-16 16:18:01 -0500

    completed image rotation and resizing via css

--- a04/scripts/rotate.js
+++ a04/scripts/rotate.js
@@ -5,9 +5,9 @@ var today = new Date();
 var prefix = "images/";
 
 //Use that prefix to put the proper sequence of image filenames into an array
-var imageArray = new Array(2);
-for (i=1; i<imageArray.length+1; i++)
-    imageArray[i-1] = prefix + i + ".png";
+var imageArray = new Array(8);
+for (i=0; i<imageArray.length; i++)
+    imageArray[i] = prefix + (i+3) + ".jpg";
 console.log(imageArray);
 
 //Perform a "cicular rotation" of the images in the array
@@ -17,7 +17,7 @@ function rotate()
     var imageObject = document.getElementById('placeholder');
     imageObject.src = imageArray[imageCounter];
     ++imageCounter;
-    if (imageCounter == 2) imageCounter = 0;
+    if (imageCounter == 8) imageCounter = 0;
 }
 
 //Called as soon as home page has loaded, to start image rotation
